b07377ff4a928223cccb98c0ba05f31a13bcb984
jpconver <jpconver@gmail.com>
2021-08-09 18:42:36 -0300

    add docker-compose examples

---
+++ dockerCompose1/src/index.php
@@ -0,0 +1,18 @@
+<?php
+echo "<html>";
+$conn = new mysqli("mysql", "db_user", "db_password", "my_db");
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT name FROM user";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    echo $row['name']."<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+echo "</html>";
+?>
